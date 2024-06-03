13873242beeaee3e8d25b3f66a57f82d75a0994f
m-tkd <m-takeda731@outlook.jp>
2024-06-03 18:55:15 +0900

    商品登録まで作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Models\Item;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
@@ -13,7 +14,11 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::select('id', 'name', 'price', 'is_selling')->get();
+
+        return Inertia::render('Items/Index', [
+            'items' => $items
+        ]);
     }
 
     /**
@@ -21,7 +26,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
@@ -29,7 +34,13 @@ class ItemController extends Controller
      */
     public function store(StoreItemRequest $request)
     {
-        //
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+
+        return to_route('items.index');
     }
 
     /**
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ItemSeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'カット',
+                'memo' => 'カットの詳細',
+                'price' => 6000
+            ],
+            [
+                'name' => 'カラー',
+                'memo' => 'カラーの詳細',
+                'price' => 8000
+            ],
+            [
+                'name' => 'パーマ（カット込み）',
+                'memo' => 'パーマの詳細',
+                'price' => 13000
+            ],
+        ]);
     }
 }
