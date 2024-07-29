c77ac8057b11e41f88181802dd37a8cdacb04d2c
m-tkd <m-takeda731@outlook.jp>
2024-07-29 22:22:15 +0900

    aauth:sanctumをグループ化

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/searchCustomers', function (Request $request) {
-    return Customer::searchCustomers($request->search)
-        ->select('id', 'name', 'kana', 'tel')->paginate();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/searchCustomers', function (Request $request) {
+        return Customer::searchCustomers($request->search)
+            ->select('id', 'name', 'kana', 'tel')->paginate();
+    });
+});
